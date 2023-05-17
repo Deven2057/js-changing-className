1ef6163984bffdc95fe9d56315042b3185ed1239
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-05-17 17:57:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+first.className="yellow black-text"
+first.className="yellow "
+ first.className="red black-text"
+ first.className="red "
+first.classList.remove('red')
+first.classList.addClass('red')
+first.classList.toggle('red')
+first.classList.contains('red')
